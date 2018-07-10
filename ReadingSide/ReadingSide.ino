b11793485e9203b88f1fb3dc2b0da79d85647c38
Jasmine Chiang <jasmineychiang@gmail.com>
2018-07-10 10:46:29 -0700

    Reworked code

--- ReadingSide/ReadingSide.ino
+++ ReadingSide/ReadingSide.ino
@@ -14,7 +14,6 @@ struct Packet {
   int value3;
   int value4;
   int value5;
-  int checkSum;
 } pkt;
  
 void setup() {
@@ -31,29 +30,22 @@ void loop() {
   // put your main code here, to run repeatedly:
   if(radio.rfAvailable()) {
     radio.rfRead((uint8_t *) & pkt, sizeof(Packet));
+    radio.rfFlush();
   }
-  int checkSum1 = pkt.value1 + pkt.value2 + pkt.value3 + pkt.value4 + pkt.value5;
-  Serial.print("checkSum:");
-  Serial.println(checkSum1);
-  Serial.println("CHECKING");
-  if(checkSum1 == pkt.checkSum) {
-    Serial.println("READING RECEIVED PACKET");
-    Serial.println(pkt.value1);
-    Serial.println(pkt.value2);
-    Serial.println(pkt.value3);
-    Serial.println(pkt.value4);
-    Serial.println(pkt.value5);
- 
-    servo1.write(pkt.value1); //turn to value given by flex sensor
-    servo2.write(pkt.value2);
-    servo3.write(pkt.value3);
-    servo4.write(pkt.value4);
-    servo5.write(pkt.value5);
-  }
-  else {
-    Serial.println("ERROR");
-  }
+  Serial.println("READING RECEIVED PACKET");
+  Serial.println(pkt.value1);
+  Serial.println(pkt.value2);
+  Serial.println(pkt.value3);
+  Serial.println(pkt.value4);
+  Serial.println(pkt.value5);
   Serial.println("");
+  
+  servo1.write(pkt.value1); //turn to value given by flex sensor
+  servo2.write(pkt.value2);
+  servo3.write(pkt.value3);
+  servo4.write(pkt.value4);
+  servo5.write(pkt.value5);
+  
   delay(500);
 }
 
